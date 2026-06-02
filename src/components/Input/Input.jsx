9706feef4c9aad538b051194837eec8bcf5ff77b
ShreeGattani <gattanishree31@gmail.com>
2026-06-02 12:27:11 +0530

    feat: complete Day 2 input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,51 @@
-import './Input.css';
+import React from "react";
+import "./Input.css";
 
-export const Input = () => {
-  return <div></div>;
+export const Input = ({
+  placeholder = "",
+  defaultValue = "",
+  onChange,
+  error,
+  disabled = false,
+  leftIcon,
+  rightIcon,
+  className = "",
+  ...props
+}) => {
+  const handleChange = (e) => {
+    if (!disabled && onChange) {
+      onChange(e);
+    }
+  };
+
+  return (
+    <div className="input-wrapper">
+      {leftIcon && <span className="left-icon">{leftIcon}</span>}
+
+      <input
+        type="text"
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        onChange={handleChange}
+        className={[
+          "input",
+          error && "input-error",
+          disabled && "input-disabled",
+          className,
+        ]
+          .filter(Boolean)
+          .join(" ")}
+        {...props}
+      />
+
+      {rightIcon && <span className="right-icon">{rightIcon}</span>}
+
+      {error && (
+        <div role="alert" className="error-message">
+          {error}
+        </div>
+      )}
+    </div>
+  );
 };
