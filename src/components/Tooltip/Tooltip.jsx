ad57fc9db1522c166fac6170117b5b420866bb81
ShreeGattani <gattanishree31@gmail.com>
2026-06-09 22:56:06 +0530

    feature/day7-tooltip

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,5 +1,24 @@
 import './Tooltip.css';
+import React, { useState } from "react";
 
-export const Tooltip = () => {
-  return <div></div>;
-};
+export function Tooltip({children , content}){
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+    className= "tooltip-wrapper"
+    onMouseEnter={() => setIsVisible(true)}
+    onMouseLeave={() => setIsVisible(false)}
+    onFocus={() => setIsVisible(true)}
+    onBlur={() => setIsVisible(false)}
+    >
+      {children}
+
+      {isVisible && (
+        <div className ="tooltip-content" role= "tooltip">
+          {content}
+          </div>
+      )}
+    </div>
+  )  
+}
